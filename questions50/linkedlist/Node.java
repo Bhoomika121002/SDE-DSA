fa0dd466c164ca9ab2d1cc98b236a41416bdaec7
Bhoomika121002 <chaudharybhoomika12@gmail.com>
2023-08-31 23:38:18 +0530

    commit

--- questions50/linkedlist/Node.java
+++
@@ -1,7 +0,0 @@
-package questions50.linkedlist;
-
-public class Node {
-
-    public Node next;
-
-}
--- questions50/linkedlist/middleelement.java
+++ questions50/linkedlist/middleelement.java
@@ -3,9 +3,9 @@ package questions50.linkedlist;
 public class middleelement {
     public static void main(String[] args) {
         class Solution {
-            public Node middleNode(Node head) {
-                Node slow = head; 
-                Node fast = head; 
+            public startpoint middleNode(startpoint head) {
+                startpoint slow = head; 
+                startpoint fast = head; 
                 while(fast != null && fast.next != null ){
                     slow = slow.next;
                     fast = fast.next.next;
