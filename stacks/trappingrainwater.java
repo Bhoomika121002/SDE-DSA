afb758578150128c7f7dd59d89be9b1defd4dd07
Bhoomika121002 <chaudharybhoomika12@gmail.com>
2023-09-13 22:45:43 +0530

    commit

--- stacks/trappingrainwater.java
+++ stacks/trappingrainwater.java
@@ -11,7 +11,7 @@ public class trappingrainwater {
         }
         right[n-1] = a[n-1];
         for(int i = n-2; i>=0 ; i--){
-            right[i] = Math.max (right[i+1],a[i]);
+            right[i] = Math.max (right[i+1],a[i]);  //
         }
         int ans = 0; 
         for(int i =0; i<n; i++){
